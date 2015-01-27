e4ea60672cd7ff45ed2a280b417c215050adb8aa
Olivier Dusabimana <diokeyolivier@gmail.com>
2015-01-26 19:36:30 -0500

    Mean Stack boiler plate

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('meanStarterApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
